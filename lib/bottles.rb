f102bacf95a843b654d3bfff80f79d63a26f9071
Chris Tucker Mear <chris@feedmechocolate.com>
2019-03-26 09:56:05 +0000

    Implement another verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,10 +2,10 @@
 
 # Returns the lyrics to a song about beer.
 class Bottles
-  def verse(___)
-    <<~VERSE
-      99 bottles of beer on the wall, 99 bottles of beer.
-      Take one down and pass it around, 98 bottles of beer on the wall.
-    VERSE
+  def verse(verse_number)
+    "#{verse_number} bottles of beer on the wall, " \
+    "#{verse_number} bottles of beer.\n" \
+    'Take one down and pass it around, ' \
+    "#{verse_number - 1} bottles of beer on the wall.\n"
   end
 end
