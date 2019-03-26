238cee018449a0f89a7f28845d8c6875ec382327
Chris Tucker Mear <chris@feedmechocolate.com>
2019-03-26 09:53:05 +0000

    Implement the first verse

---
+++ lib/bottles.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Returns the lyrics to a song about beer.
+class Bottles
+  def verse(___)
+    <<~VERSE
+      99 bottles of beer on the wall, 99 bottles of beer.
+      Take one down and pass it around, 98 bottles of beer on the wall.
+    VERSE
+  end
+end
