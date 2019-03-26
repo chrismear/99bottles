50295b754df97070456292109ae41a7d8be90e18
Chris Tucker Mear <chris@feedmechocolate.com>
2019-03-26 09:58:45 +0000

    Implement verse 2

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,9 +3,19 @@
 # Returns the lyrics to a song about beer.
 class Bottles
   def verse(verse_number)
-    "#{verse_number} bottles of beer on the wall, " \
-    "#{verse_number} bottles of beer.\n" \
+    "#{bottles(verse_number)} on the wall, " \
+    "#{bottles(verse_number)}.\n" \
     'Take one down and pass it around, ' \
-    "#{verse_number - 1} bottles of beer on the wall.\n"
+    "#{bottles(verse_number - 1)} on the wall.\n"
+  end
+
+  private
+
+  def bottles(number)
+    if number == 1
+      '1 bottle of beer'
+    else
+      "#{number} bottles of beer"
+    end
   end
 end
