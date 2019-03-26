46f4f7b1a72d1a5161da59d057f1ebfd4b96a919
Chris Tucker Mear <chris@feedmechocolate.com>
2019-03-26 10:01:02 +0000

    Implement verse 1

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,19 +3,29 @@
 # Returns the lyrics to a song about beer.
 class Bottles
   def verse(verse_number)
-    "#{bottles(verse_number)} on the wall, " \
-    "#{bottles(verse_number)}.\n" \
-    'Take one down and pass it around, ' \
-    "#{bottles(verse_number - 1)} on the wall.\n"
+    "#{bottles_of_beer(verse_number)} on the wall, " \
+    "#{bottles_of_beer(verse_number)}.\n" \
+    "Take #{indefinite_pronoun(verse_number)} down and pass it around, " \
+    "#{bottles_of_beer(verse_number - 1)} on the wall.\n"
   end
 
   private
 
-  def bottles(number)
+  def bottles_of_beer(number)
     if number == 1
       '1 bottle of beer'
+    elsif number.zero?
+      'no more bottles of beer'
     else
       "#{number} bottles of beer"
+    end
+  end
+
+  def indefinite_pronoun(number)
+    if number == 1
+      'it'
+    else
+      'one'
     end
   end
 end
